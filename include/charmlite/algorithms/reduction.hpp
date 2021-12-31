0387d5dfb2d21c39c2a48d80d1677bdb28dd64e6
Justin Szaday <jjszaday@gmail.com>
2021-12-30 20:29:27 -0600

    Remove Converse Combiner

--- include/charmlite/algorithms/reduction.hpp
+++ include/charmlite/algorithms/reduction.hpp
@@ -4,24 +4,15 @@
 #include <charmlite/core/callback.hpp>
 #include <charmlite/core/message.hpp>
 
-// TODO ( converse collectives should be isolated/removed )
+/* collection of combiners for
+ * reduction operations
+ *
+ * TODO( support wrapping
+ *       std::plus and their ilk )
+ *
+ */
 
 namespace cmk {
-    void* converse_combiner_(int* size, void* local, void** remote, int count);
-
-    template <typename Message, combiner_fn_t<Message> Combiner,
-        callback_fn_t<Message> Callback>
-    void reduce(message_ptr<Message>&& msg)
-    {
-        // callback will be invoked on pe0
-        new (&msg->dst_)
-            destination(callback_helper_<Message, Callback>::id_, 0);
-        msg->has_combiner() = true;
-        *(msg->combiner()) = combiner_helper_<Message, Combiner>::id_;
-        auto sz = msg->total_size_;
-        CmiReduce(msg.release(), sz, converse_combiner_);
-    }
-
     template <typename T = message>
     message_ptr<T> nop(message_ptr<T>&& msg, message_ptr<T>&&)
     {
