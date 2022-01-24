3219c790ebeac587642a320cd4daaf2072a2e7bb
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2022-01-24 10:17:12 -0800

    Add infinite dimensional index range support to charmlite (#36)
    * Add infinite dimensions to index_view and index_range
    
    * Ensure number of chares are correctly created
    
    * Fix Tuple Indexing
    
    Co-authored-by: Justin Szaday <jjszaday@gmail.com>

---
+++ tests/indexing/compile_time.cpp
@@ -0,0 +1,19 @@
+#include <charmlite/core/indexing.hpp>
+
+#include <tuple>
+
+int main()
+{
+    // 1D range
+    constexpr cmk::index_range<int> index_range_1d{0, 1, 5};
+    constexpr int next_1d = index_range_1d.next(0);
+    static_assert(next_1d == 1, "Next computed incorrectly for 1D range!");
+
+    constexpr cmk::index_range<std::tuple<int, int>> index_range_2d{
+        std::tuple<int, int>{0, 0}, std::tuple<int, int>{0, 1},
+        std::tuple<int, int>{2, 2}};
+    constexpr std::tuple<int, int> next_2d =
+        index_range_2d.next(std::make_tuple(0, 0));
+    static_assert(std::get<0>(next_2d) == 0 && std::get<1>(next_2d) == 0,
+        "Next computed incorrectly for 2D range!");
+}
