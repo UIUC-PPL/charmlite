07fb7d8a0ed69d61c28efaf43cc8ee9f170c5b8e
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2022-02-01 19:53:16 -0800

    Add parameter marshalling to hello (#43)

--- examples/hello/hello.cpp
+++ examples/hello/hello.cpp
@@ -5,7 +5,6 @@
  * TODOs:
  * - we can add something like `cmk::do_once`
  * - we can add something like `CkCallback(CkCallback::ckExit)`
- * - parameter marshalling will help!
  * - a `nop`-by-default contribute op will help!
  */
 
@@ -14,11 +13,12 @@
 class hello : public cmk::chare<hello, int>
 {
 public:
-    void sayHello(cmk::message_ptr<>&& msg)
+    void sayHello()
     {
         CmiPrintf("%d> hello!\n", this->index());
 
-        this->element_proxy().contribute<cmk::nop<>>(std::move(msg),
+        this->element_proxy().contribute<cmk::nop<>>(
+            cmk::make_message<cmk::message>(),
             cmk::callback<cmk::message>::construct<&cmk::exit>(cmk::all::pes));
     }
 };
@@ -30,7 +30,7 @@ int main(int argc, char** argv)
     if (CmiMyNode() == 0)
     {
         auto proxy = cmk::group_proxy<hello>::construct();
-        proxy.broadcast<&hello::sayHello>(cmk::make_message<cmk::message>());
+        proxy.broadcast<&hello::sayHello>();
     }
 
     cmk::finalize();
